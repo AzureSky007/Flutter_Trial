8a3224e508c326c65d4ff0f5cece978821da0fe0
Aaryan Bhagat <bhagataaryan2004@gmail.com>
2023-09-05 23:41:09 +0530

    first commit

---
+++ trial.dart
@@ -0,0 +1,41 @@
+void main() {
+  // Data Types
+  String greet = 'Hello World';
+  print(greet);
+
+  int num = 30;
+  print(num);
+
+  double num2 = 41.23;
+  print(num2);
+
+  bool value = false;
+  print(value);
+
+  List items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+  print(items);
+
+  Map items2 = {'Item 1': 'Item 1', 'Item 2': 2, 'Item 3': 30.23};
+  print(items2);
+
+  // Variables
+  String name = 'Sky';
+  // print('');
+  print(name);
+
+  // name = 1;
+  // Line above not possible because it is static
+
+  int nums = 20;
+  print(nums);
+
+  dynamic x = 'Sky';
+  x = 30;
+  print(x);
+
+  final value1 = 12;
+  print(value1);
+
+  const const1 = 'Dart Programming';
+  print(const1);
+}
